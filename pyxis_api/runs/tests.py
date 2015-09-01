2134ea8f6499d817b1832f58f5bc63b25f9f8564
Dhiana Deva <dhiana.deva@gmail.com>
2015-09-01 13:08:09 -0300

    @dhiana | @Juraci - Adds test with expectations on the Runs API

--- pyxis_api/runs/tests.py
+++ pyxis_api/runs/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from runs.models import Run
 
 # Create your tests here.
+
+class RunsApiTestCase(TestCase):
+
+    def setUp(self):
+        # Arrange
+        run = Run.objects.create(id='1234', passes=80, fails=12, skips=8)
+
+    def test_it_should_get_all_runs(self):
+        # Act
+        response = self.client.get('/runs')
+        # Assert
+        self.assertEqual(response.status_code, 200)
