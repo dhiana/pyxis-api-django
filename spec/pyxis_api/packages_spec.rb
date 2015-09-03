ac0b804c722ec7c1e7c8130880bf29654534e51b
Juraci <juraci.vieira@gmail.com>
2015-09-03 10:19:22 -0300

    @dhiana | @Juraci -  esnsures python-dev package is present

--- spec/pyxis_api/packages_spec.rb
+++ spec/pyxis_api/packages_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'required packages' do
-  @packages = ['mysql-client', 'python-pip']
+  @packages = ['mysql-client', 'python-pip', 'python-dev']
 
   @packages.each do |package|
     describe package(package) do
